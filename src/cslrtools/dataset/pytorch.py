167fd171000295ef1dc3210975897e4503231d25
ikeda <ikeda@lee-lab.org>
2025-06-10 20:36:13 +0900

    test

--- src/cslrtools/dataset/pytorch.py
+++ src/cslrtools/dataset/pytorch.py
@@ -76,8 +76,8 @@ class Dataset(torch.utils.data.Dataset[DataTuple], Generic[_M]):
         inputs_mean = (sample_mean * sample_len).sum(0) / sample_len_sum
         inputs_var = (sample_var * sample_len).sum(0) / sample_len_sum
 
-        print(inputs_mean)
-        print(inputs_var)
+        print((sample_mean * sample_len).sum(0))
+        print((sample_var * sample_len).sum(0))
 
         labels_set = {blank_label} | set(chain.from_iterable(labels))
         ordered_labels = sorted(labels_set)
